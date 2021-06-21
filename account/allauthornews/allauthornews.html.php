10a8fe737c78936d445f54ea2b48e2532a7eb73f
MrCasades <casades@list.ru>
2021-06-22 00:31:17 +0300

    do accaunt

--- account/allauthornews/allauthornews.html.php
+++ account/allauthornews/allauthornews.html.php
@@ -4,52 +4,36 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont_for_view"> 
-			<div align = "center"><script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-			<script src="//yastatic.net/share2/share.js"></script>
-			<div class="ya-share2" data-services="collections,vkontakte,facebook,odnoklassniki,moimir,twitter,lj"></div></div>
-		
-		<div>
-		 <?php 
-		 if (empty ($newsIn))
-		 {
-			 echo '<p align = "center">Новости у автора отсутствуют!</p>';
-		 }
+
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class = "main-post m-content">
+	<?php if (empty($newsIn))
+	 {
+		echo '<p class = "m-content">Новости у автора отсутствуют!</p>';
+	 }
 		 
-		 else
+	 else
 			 
-		 foreach ($newsIn as $news): ?> 
-		  
-			<div>
-				
-				<div class = "post">
-				  <div class = "posttitle">
-				    <?php echo ($news['newsdate']. ' | Автор: <a href="../../account/?id='.$news['idauthor'].'" style="color: white" >'.$news['authorname']).'</a>';?>
-					<p>Рубрика: <a href="../../viewcategory/?id=<?php echo $news['categoryid']; ?>" style="color: white"><?php echo $news['categoryname'];?></a></p>
-				  </div>
-				  <div class = "newstext">
-					<h3 align = "center"><?php htmlecho ($news['newstitle']); ?></h3>
-					<div class = "newsimg">
-					   <?php if ($news['imghead'] == '')
-						{
-							$img = '';//если картинка в заголовке отсутствует
-							echo $img;
-						}
-						 else 
-						{
-							$img = '<img width = "90%" height = "90%" src="../../images/'.$news['imghead'].'"'. ' alt="'.$news['imgalt'].'"'.'>';//если картинка присутствует
-						}?>
-					<p><?php echo $img;?></p>
-				   </div>
-					<p><?php echomarkdown (implode(' ', array_slice(explode(' ', strip_tags($news['textnews'])), 0, 50))); ?> [...]</p>
-					<p><a href="../../viewnews/?id=<?php htmlecho ($news['id']); ?>" class="btn btn-primary">Далее</a></p>
-				  </div>
-				 </div> 
-			</div>			
-		 <?php endforeach; ?>
+	 foreach ($newsIn as $news): ?>
+    <a href="../../viewnews/?id=<?php htmlecho ($news['id']); ?>" class = "post-place-2" style="background-image: url(../../images/<?php echo $news['imghead']; ?>)">
+        <div class = "post-top-1">
+            <p><?php echo date("Y.m.d H:i", strtotime($news['newsdate'])); ?></p>
+			<span class="post-rubrics"><?php htmlecho ($news['categoryname']); ?></span>
+        </div>
+        <div class = "post-bottom-1"><?php htmlecho ((implode(' ', array_slice(explode(' ', strip_tags($news['newstitle'])), 0, 7)))); ?>...</div>
+	</a>
 		
-		<div align = "center">	
+	<?php endforeach; ?>
+	
+</div>
+
+<div class="page-output">	
 		 <?php
 			/*Постраничный вывод информации*/
 			for ($i = 1; $i <= $pagesCount; $i++) 
@@ -57,17 +41,15 @@ include_once MAIN_FILE . '/header.inc.php';?>
 				// если текущая старница
 				if($i == $page)
 				{
-					echo "<a href='index.php?page=$i&id=$idAuthor' class='btn btn-info'>$i</a> ";
+					echo "<a href='index.php?page=$i&id=$idAuthor'><button class='btn_2'>$i</button></a> ";
 				} 
 				else 
 				{
-					echo "<a href='index.php?page=$i&id=$idAuthor' class='btn btn-primary btn-sm'>$i</a> ";
+					echo "<a href='index.php?page=$i&id=$idAuthor'><button class='btn_1'>$i</button></a> ";
 				}
 			}?>
-		</div>	
-		</div>
-	</div>		
-
+</div>	
+	
 <?php 
 /*Загрузка footer*/
 include_once MAIN_FILE . '/footer.inc.php';?>
--- account/allauthorpost/allauthorpost.html.php
+++ account/allauthorpost/allauthorpost.html.php
@@ -4,70 +4,52 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont_for_view"> 
-			<div align = "center"><script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-			<script src="//yastatic.net/share2/share.js"></script>
-			<div class="ya-share2" data-services="collections,vkontakte,facebook,odnoklassniki,moimir,twitter,lj"></div></div>
-		
-		<div>
-		 <?php 
-		 if (empty ($posts))
-		 {
-			 echo '<p align = "center">Статьи у автора отсутствуют!</p>';
-		 }
-		 
-		 else
-			 
-		 foreach ($posts as $post): ?> 
-		  
-			<div>
-				
-				<div class = "post">
-				  <div class = "posttitle">
-				    <?php echo ($post['postdate']. ' | Автор: <a href="../../account/?id='.$post['idauthor'].'" style="color: white" >'.$post['authorname']).'</a>';?>
-					<p>Рубрика: <a href="../../viewcategory/?id=<?php echo $post['categoryid']; ?>" style="color: white"><?php echo $post['categoryname'];?></a></p>
-				  </div>
-				  <div class = "newstext">
-					<h3 align = "center"><?php htmlecho ($post['posttitle']); ?></h3>
-					 <div class = "newsimg">
-					   <?php if ($post['imghead'] == '')
-						{
-							$img = '';//если картинка в заголовке отсутствует
-							echo $img;
-						}
-						 else 
-						{
-							$img = '<img width = "90%" height = "90%" src="../../images/'.$post['imghead'].'"'. ' alt="'.$post['imgalt'].'"'.'>';//если картинка присутствует
-						}?>
-					<p><?php echo $img;?></p>
-				   </div>
-					<p><?php echomarkdown (implode(' ', array_slice(explode(' ', strip_tags($post['text'])), 0, 50))); ?> [...]</p>
-					<p><a href="../../viewpost/?id=<?php htmlecho ($post['id']); ?>" class="btn btn-primary">Далее</a></p>
-				  </div>
-				 </div> 
-			</div>			
-		 <?php endforeach; ?>
-		
-		<div align = "center">	
-		 <?php
-			/*Постраничный вывод информации*/
-			for ($i = 1; $i <= $pagesCount; $i++) 
-			{
-				// если текущая старница
-				if($i == $page)
-				{
-					echo "<a href='index.php?page=$i&id=$idAuthor' class='btn btn-info'>$i</a> ";
-				} 
-				else 
-				{
-					echo "<a href='index.php?page=$i&id=$idAuthor' class='btn btn-primary btn-sm'>$i</a> ";
-				}
-			}?>
-		</div>	
-		</div>
-	</div>		
 
+	<div class = "main-headers">
+		<div class = "headers-places"> 
+			<div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+		</div>
+		<div class = "main-headers-line"></div>
+	</div>
+
+	<div class = "main-post m-content">
+		<?php if (empty($posts))
+		{
+			echo '<p align = "center">Статьи отсутствуют</p>';
+		}
+			
+		else
+				
+		foreach ($posts as $post): ?>
+		<a href="../../viewpost/?id=<?php htmlecho ($post['id']); ?>" class = "post-place-2" style="background-image: url(../../images/<?php echo $post['imghead']; ?>)">
+			<div class = "post-top-1">
+				<p><?php echo date("Y.m.d H:i", strtotime($post['postdate'])); ?></p>
+				<span class="post-rubrics"><?php htmlecho ($post['categoryname']); ?></span>
+			</div>
+			<div class = "post-bottom-1"><?php htmlecho ((implode(' ', array_slice(explode(' ', strip_tags($post['posttitle'])), 0, 7)))); ?>...</div>
+		</a>
+			
+		<?php endforeach; ?>
+		
+	</div>
+
+	<div class="page-output">	
+			<?php
+				/*Постраничный вывод информации*/
+				for ($i = 1; $i <= $pagesCount; $i++) 
+				{
+					// если текущая старница
+					if($i == $page)
+					{
+						echo "<a href='index.php?page=$i&id=$idAuthor'><button class='btn_2'>$i</button></a> ";
+					} 
+					else 
+					{
+						echo "<a href='index.php?page=$i&id=$idAuthor'><button class='btn_1'>$i</button></a> ";
+					}
+				}?>	
+	</div>
+	
 <?php 
 /*Загрузка footer*/
 include_once MAIN_FILE . '/footer.inc.php';?>
