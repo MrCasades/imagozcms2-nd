350a4863bf9d751647d704f4a801bfca016a79f7
MrCasades <casades@list.ru>
2021-06-23 00:31:32 +0300

    do favorites

--- account/viewallfavourites/viewallfavourites.html.php
+++ account/viewallfavourites/viewallfavourites.html.php
@@ -4,53 +4,37 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont_for_view"> 
-			<div align = "center"><script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-			<script src="//yastatic.net/share2/share.js"></script>
-			<div class="ya-share2" data-services="collections,vkontakte,facebook,odnoklassniki,moimir,twitter,lj"></div></div>
-		
-		<div>
-		 <?php 
-		 if (empty ($favourites)) 
-				{
-					echo '<p align = "center">Материалы отсутствуют</p>';
-					$favourites = '';
-				}
-		 	
-			else
+
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class = "main-post m-content">
+	<?php if (empty($favourites))
+	 {
+		echo '<p align = "center">Статьи отсутствуют</p>';
+		$favourites = '';
+	 }
 		 
-		 foreach ($favourites as $favourite): ?> 
-		  
-			<div>
-				
-				<div class = "post">
-				  <div class = "posttitle">
-				    <?php echo ($favourite['date']. ' | Автор: <a href="../../account/?id='.$favourite['idauthorpost'].'" style="color: white" >'.$favourite['authorname']).'</a>';?>
-					<p>Рубрика: <a href="../../viewcategory/?id=<?php echo $favourite['idcategory']; ?>" style="color: white"><?php echo $favourite['categoryname'];?></a></p> 
-				  </div>
-				  	 
-				   <div class = "newstext">
-				    <h3 align = "center"><?php htmlecho ($favourite['title']); ?></h3>
-				    <div class = "newsimg">
-					   <?php if ($favourite['imghead'] == '')
-						{
-							$img = '';//если картинка в заголовке отсутствует
-							echo $img;
-						}
-						 else 
-						{
-							$img = '<img width = "90%" height = "90%" src="../../images/'.$favourite['imghead'].'"'. ' alt="'.$favourite['imgalt'].'"'.'>';//если картинка присутствует
-						}?>
-					  <p><?php echo $img;?></p>
-				     </div>
-					<p align = "justify"><?php echomarkdown ($favourite['post']); ?> [...]</p>
-					<?php echo $favourite['url']; ?> 
-				   </div>	
-				 </div>
-			<?php endforeach; ?>
+	 else
+			 
+	 foreach ($favourites as $favourite): ?>
+    <a href="<?php htmlecho ($favourite['url']); ?>" class = "post-place-2" style="background-image: url(../../images/<?php echo $favourite['imghead']; ?>)">
+        <div class = "post-top-1">
+            <p><?php echo date("Y.m.d H:i", strtotime($favourite['date'])); ?></p>
+			<span class="post-rubrics"><?php htmlecho ($favourite['categoryname']); ?></span>
+        </div>
+        <div class = "post-bottom-1"><?php htmlecho ((implode(' ', array_slice(explode(' ', strip_tags($favourite['title'])), 0, 7)))); ?>...</div>
+	</a>
 		
-		<div align = "center">	
+	<?php endforeach; ?>
+	
+</div>
+
+<div class="page-output">	
 		 <?php
 			/*Постраничный вывод информации*/
 			for ($i = 1; $i <= $pagesCount; $i++) 
@@ -58,17 +42,15 @@ include_once MAIN_FILE . '/header.inc.php';?>
 				// если текущая старница
 				if($i == $page)
 				{
-					echo "<a href='index.php?page=$i&id=$idAuthor' class='btn btn-info'>$i</a> ";
+					echo "<a href='index.php?page=$i&id=$idAuthor'><button class='btn_2'>$i</button></a> ";
 				} 
 				else 
 				{
-					echo "<a href='index.php?page=$i&id=$idAuthor' class='btn btn-primary btn-sm'>$i</a> ";
+					echo "<a href='index.php?page=$i&id=$idAuthor'><button class='btn_1'>$i</button></a> ";
 				}
 			}?>
-		</div>	
-		</div>
-	</div>		
-
+</div>	
+	
 <?php 
 /*Загрузка footer*/
 include_once MAIN_FILE . '/footer.inc.php';?>
