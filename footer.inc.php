760ae4fc41486dc6bf440850b626c4c60afe4a30
MrCasades <casades@list.ru>
2021-06-24 00:29:02 +0300

    new error-message

--- footer.inc.php
+++ footer.inc.php
@@ -6,16 +6,16 @@
 		<p><a href="<?php echo '//'.MAIN_URL;?>/sitemap/">Карта сайта</a></p>
 		<p>Copyright © 2019 MrCasades. All rights reserved.</p>
 	</div>
-		
-		<?php 
-			//Дополнительный код
-			echo $scriptJScode = $scriptJScode ?? ''; ?>
+  </footer>	
+
+  <?php 
+		//Дополнительный код
+		echo $scriptJScode = $scriptJScode ?? ''; ?>
 			
-		<script src="<?php echo '//'.MAIN_URL.'/jquery-3.5.1.min.js';?>"></script>
-        <script src="<?php echo '//'.MAIN_URL.'/anime.min.js';?>"></script>
-        <script src="<?php echo '//'.MAIN_URL.'/OwlCarousel/dist/owl.carousel.min.js';?>"></script>
-		<script src="<?php echo '//'.MAIN_URL.'/script-nd.js';?>"></script>	
-  </footer>			
+	<script src="<?php echo '//'.MAIN_URL.'/jquery-3.5.1.min.js';?>"></script>
+    <script src="<?php echo '//'.MAIN_URL.'/anime.min.js';?>"></script>
+    <script src="<?php echo '//'.MAIN_URL.'/OwlCarousel/dist/owl.carousel.min.js';?>"></script>
+	<script src="<?php echo '//'.MAIN_URL.'/script-nd.js';?>"></script>			
 
 </body>
 </html>	
