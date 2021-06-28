d09bb9ba3e4c23bb94f958659710f196b41d3f5c
MrCasades <casades@list.ru>
2021-06-29 00:28:17 +0300

    DO MESSAGES

--- mainmessages/addupdmainmessage/addupdform.html.php
+++ mainmessages/addupdmainmessage/addupdform.html.php
@@ -4,44 +4,40 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont">
-	<div class = "post">
-	
-	<p align = "center"><strong><?php htmlecho($errorForm); ?></strong></p>
+
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content form-pl">
+	<p class="error-log"><?php htmlecho($errorForm); ?></p>
 	
 	<form action = "?<?php htmlecho($action); ?> " method = "post" enctype="multipart/form-data" autocomplete="on">
 	<table>
-	 <div>
 	  <tr>
-		<td><label for = "author"> Автор:</label></td>
+		<td><strong> Автор:</strong></td>
 		<td>
 		 <?php echo $authorPost;?>
 		</td>
 	  </tr>
-	 </div>	 
-	 <div>
 	  <tr>
-		<td><label for = "upload">Загрузите файл изображения</label><input type = "file" name = "upload" id = "upload"></td>
+		<td><strong>Загрузите файл изображения</strong><input type = "file" name = "upload" id = "upload"></td>
 		<td><input type = "hidden" name = "action" value = "upload"></td>
 	  </tr>		 
-	</div>
 	</table>	
-	 <div>
-		<label for = "promotion">Введите текст сообщения</label><br>
-		<textarea class = "descr" id = "text" name = "text" data-provide="markdown" rows="10"><?php htmlecho($text);?></textarea>	
-	 </div>
-	 <hr/>
-	  <div>
+		<p><strong>Введите текст сообщения</strong></p>
+		<textarea class = "descr" id = "text" name = "text" data-provide="markdown" rows="10"><?php htmlecho($text);?></textarea>	 
 		<input type = "hidden" name = "idto" value = "<?php htmlecho($idto); ?>">
 		<input type = "hidden" name = "id" value = "<?php htmlecho($id); ?>">
-		<input type = "submit" value = "<?php htmlecho($button); ?>" class="btn btn-primary btn-sm">
-	  </div>	
+		<p><input type = "submit" value = "<?php htmlecho($button); ?>" class="btn_2 addit-btn">
+		<a href="#" onclick="history.back();"><button type="button" class="btn_1 addit-btn">Назад</button></a></p>	
 	</form>	
 	<p><a name="bottom"></a></p>	
-	</div>
-	</div>
-
+</div>
+	
 <?php 
 /*Загрузка footer*/
 include_once MAIN_FILE . '/footer.inc.php';?>	
