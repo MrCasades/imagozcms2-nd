54d771b485a9c8d6b3eade704733ce5880a09685
MrCasades <casades@list.ru>
2021-06-30 00:43:48 +0300

    DO accsessgood

--- admin/accessgood.html.php
+++ admin/accessgood.html.php
@@ -5,13 +5,18 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
 
-	<div class = "maincont">
-	 <div class = "post">
-	  <p align="center"><?php htmlecho($loggood); ?> </p>
-	  <div align="center"><a href="#" onclick="history.go(-2);" class="btn btn-primary btn-sm">Назад</a></div>
-	 </div>
-	</div>		
-		
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class = "error-pl">
+	<p class="for-info-txt"><?php htmlecho($loggood); ?></p>
+	<p><a href="#" onclick="history.go(-2);"><button class="btn_2">Назад</button></a></p>
+</div>
+	
 <?php 
 /*Загрузка footer*/
 include_once MAIN_FILE . '/footer.inc.php';?>
