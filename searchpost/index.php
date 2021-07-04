62579b7173e2a93ccad95e162a363c35b91bacd6
MrCasades <38098164+MrCasades@users.noreply.github.com>
2021-07-04 18:24:34 +0300

    do messenger

--- searchpost/index.php
+++ searchpost/index.php
@@ -4,8 +4,8 @@ include_once '../includes/path.inc.php';
 
 /*Вывод информации для формы поиска*/
 
-$title = 'Поиск статей | imagoz.ru';
-$headMain = 'Поиск статей';
+$title = 'Поиск публикаций | imagoz.ru';
+$headMain = 'Поиск публикаций';
 $robots = 'noindex, follow';
 $descr = 'В данном разделе осуществляется поиск информации';
 	
@@ -17,115 +17,14 @@ if (loggedIn())
 	/*Если loggedIn = TRUE, выводится имя пользователя иначе меню авторизации*/
 }
 
-/*Формирование запроса SELECT*/
-	
-if (isset($_GET['action']) && ($_GET['action']) == 'search')
-{
-	/*Подключение к базе данных*/
-	include MAIN_FILE . '/includes/db.inc.php';
-		
-	/*Переменные для выражения SELECT*/
-	$select = 'SELECT posts.id AS postid, post, posttitle, imghead, imgalt, postdate, authorname, author.id AS authorid, category.id AS categoryid, categoryname';
-	$from = ' FROM posts 
-			  INNER JOIN author ON idauthor = author.id 
-			  INNER JOIN category ON idcategory = category.id';
-	$where = ' WHERE TRUE AND premoderation = "YES" AND zenpost = "NO"';
-		
-	$forSearch = array();//массив заполнения запроса
-		
-	/*Выбор автора*/
-	/*
-	if ($_GET['author'] != '')//Если выбран автор
-	{
-		$where .= " AND idauthor = :idauthor";
-		$forSearch[':idauthor'] = $_GET['author'];
-	}
-	*/
-		
-	/*Выбор рубрики*/
-	if ($_GET['category'] != '')//Если выбрана рубрика
-	{
-		$where .= " AND idcategory = :idcategory";
-		$forSearch[':idcategory'] = $_GET['category'];
-	}
-		
-	/*Выбор тематики*/
-	/*
-	if ($_GET['meta'] != '')//Если выбрана тематика
-	{
-		$from .= ' INNER JOIN metapost ON posts.id = idpost';
-		$where .= " AND metapost.idmeta = :idmeta";
-		$forSearch[':idmeta'] = $_GET['meta'];
-	}
-	*/
-		
-	/*Поле строки*/
-	if ($_GET['text'] != '')//Если выбрана какая-то строка
-	{
-		$where .= " AND post LIKE :post";
-		$forSearch[':post'] = '%'. $_GET['text']. '%';	
-	}
-		
-	/*Объеденение переменных в запрос*/
-	try
-	{
-		$sql = $select.$from.$where;
-		$s = $pdo->prepare($sql);// подготавливает запрос для отправки в бд и возвр объект запроса присвоенный переменной
-		$s -> execute($forSearch);// метод дает инструкцию PDO отправить запрос MySQL. Т. к. массив $forSearch хранит значение всех псевдопеременных 
-								  // не нужно указывать их по отдельности с помощью bindValue									
-	}
-
-	catch (PDOException $e)
-	{
-		$title = 'ImagozCMS | Ошибка данных!';//Данные тега <title>
-		$headMain = 'Ошибка данных!';
-		$robots = 'noindex, nofollow';
-		$descr = '';
-		$error = 'Ошибка поиска : ' . $e -> getMessage();// вывод сообщения об ошибке в переменой $e
-		include 'error.html.php';
-		exit();
-	}
-		
-	foreach ($s as $row)
-	{
-			$posts[] =  array ('id' => $row['postid'], 'text' => $row['post'], 'posttitle' =>  $row['posttitle'], 'imghead' =>  $row['imghead'], 'imgalt' =>  $row['imgalt'],
-						'postdate' =>  $row['postdate'], 'authorname' =>  $row['authorname'], 'idauthor' =>  $row['authorid'],
-						'categoryname' =>  $row['categoryname'], 'categoryid' => $row['categoryid']);
-	}
-		
-	include 'searchpost.html.php';
-	exit();
-}
-	
 /*Подключение к базе данных*/
 include MAIN_FILE . '/includes/db.inc.php';
-	
-/*Если нужен поиск по авторам
-try
-{
-	$result = $pdo -> query ('SELECT id, authorname FROM author');
-}
-catch (PDOException $e)
-{
-	$title = 'ImagozCMS | Ошибка данных!';//Данные тега <title>
-	$headMain = 'Ошибка данных!';
-	$robots = 'noindex, nofollow';
-	$descr = '';
-	$error = 'Ошибка вывода author '. ' Error: '. $e -> getMessage();// вывод сообщения об ошибке в переменой $e
-	include 'error.html.php';
-	exit();
-}
-	
-foreach ($result as $row)
-{
-	$authors[] = array('id' => $row['id'], 'authorname' => $row['authorname']);
-}
-*/
-	
+
 try
 {
 	$result = $pdo -> query ('SELECT id, categoryname FROM category');
 }
+
 catch (PDOException $e)
 {
 	$title = 'ImagozCMS | Ошибка данных!';//Данные тега <title>
@@ -141,29 +40,7 @@ foreach ($result as $row)
 {
 	$categorys[] = array('id' => $row['id'], 'categoryname' => $row['categoryname']);
 }
-	
-/*Если нужен поиск по тегам
-try
-{
-	$result = $pdo -> query ('SELECT id, metaname FROM meta');
-}
-catch (PDOException $e)
-{
-	$title = 'ImagozCMS | Ошибка данных!';//Данные тега <title>
-	$headMain = 'Ошибка данных!';
-	$robots = 'noindex, nofollow';
-	$descr = '';
-	$error = 'Ошибка вывода meta '. ' Error: '. $e -> getMessage();// вывод сообщения об ошибке в переменой $e
-	include 'error.html.php';
-	exit();
-}
-	
-foreach ($result as $row)
-{
-	$metas[] = array('id' => $row['id'], 'metaname' => $row['metaname']);
-}
-*/
-	
+
 include 'searchform.html.php';
 exit();
 	
--- searchpost/searchform.html.php
+++ searchpost/searchform.html.php
@@ -16,7 +16,6 @@ include_once MAIN_FILE . '/header.inc.php';?>
 	<div class="search-form">
 		<form action = " " method = "get">
 			<input type = "text" name = "text" id = "text" class="search-text"/>
-			<label for = "category"> Рубрика:</label>
 			<select name = "category" id = "category" class="search-select">
 			<option value = "">Любая рубрика</option>
 				<?php foreach ($categorys as $category): ?>
@@ -25,49 +24,17 @@ include_once MAIN_FILE . '/header.inc.php';?>
 			</select>
 			<input type = "hidden" name = "action" value = "search"/>
 			<input type = "submit" value = "Найти" class="btn_2"/>
+			<p>
+				<input name="article_type" type="radio" value="posts" checked>Статья
+				<input name="article_type" type="radio" value="promotions">Промоушен
+				<input name="article_type" type="radio" value="news">Новость
+			</p>
 		</form>
 	</div>
-</div>
 
-	<div class = "maincont_for_view">
-	<div class = "post_reg_log">
-	<p> <a href="<?php echo '//'.MAIN_URL.'/searchpost/';?>" class="btn btn-info">Поиск статей</a> | 
-		<a href="<?php echo '//'.MAIN_URL.'/searchpromotion/';?>" class="btn btn-primary btn-sm">Поиск промоушен-статей</a> |
-		<a href="<?php echo '//'.MAIN_URL.'/searchnews/';?>" class="btn btn-primary btn-sm">Поиск новостей</a></p>
-	<form action = " " method = "get">
-	<p>Список статей по параметрам:</p>
-	 <table>
-	  <div>
-		<tr>
-		<td><label for = "text">Содержит текст </label></td>
-		<td><input type = "text" name = "text" id = "text"/></td>
-		</tr>
-	  </div>	 
-	 <div>
-		<tr>
-		<td><label for = "category"> По рубрике:</label></td>
-		<td>
-		<select name = "category" id = "category">
-		  <option value = "">Любая рубрика</option>
-			<?php foreach ($categorys as $category): ?>
-			 <option value = "<?php htmlecho($category['id']); ?>"><?php htmlecho($category['categoryname']); ?></option>
-			<?php endforeach; ?> 
-		</select>
-		</td>		
-		</tr>
-	 </div>	
-	  <div>
-	    <tr>
-		<td>
-		<input type = "hidden" name = "action" value = "search"/>
-		<input type = "submit" value = "Найти" class="btn btn-primary btn-sm"/>
-		</td>
-		<tr>
-	  </div>
-	 </table>	
-	</form>	
-	</div>
-	</div>
+	<div id = "search-result"></div>				
+
+</div>
 	
 <?php 
 /*Загрузка footer*/
