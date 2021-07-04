a514e0c5c2fb08527d4ad2cccfe3d22753125fd8
MrCasades <casades@list.ru>
2021-07-04 15:38:48 +0300

    do search

--- searchpost/searchform.html.php
+++ searchpost/searchform.html.php
@@ -5,6 +5,30 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
 
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content">
+	<div class="search-form">
+		<form action = " " method = "get">
+			<input type = "text" name = "text" id = "text" class="search-text"/>
+			<label for = "category"> Рубрика:</label>
+			<select name = "category" id = "category" class="search-select">
+			<option value = "">Любая рубрика</option>
+				<?php foreach ($categorys as $category): ?>
+				<option value = "<?php htmlecho($category['id']); ?>"><?php htmlecho($category['categoryname']); ?></option>
+				<?php endforeach; ?> 
+			</select>
+			<input type = "hidden" name = "action" value = "search"/>
+			<input type = "submit" value = "Найти" class="btn_2"/>
+		</form>
+	</div>
+</div>
+
 	<div class = "maincont_for_view">
 	<div class = "post_reg_log">
 	<p> <a href="<?php echo '//'.MAIN_URL.'/searchpost/';?>" class="btn btn-info">Поиск статей</a> | 
