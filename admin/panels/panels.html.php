f378a8730767c80fe9ddeaefff758c849263e5ae
MrCasades <casades@list.ru>
2021-07-13 00:27:10 +0300

    do logpanel

--- admin/panels/panels.html.php
+++ admin/panels/panels.html.php
@@ -13,13 +13,15 @@ include_once MAIN_FILE . '/header.inc.php';?>
 </div>
 
 <div class = "m-content addupd-post">
+    <a href="//<?php echo MAIN_URL;?>/admin/refused/#bottom' class='btn btn-danger btn-sm"><strong>ОТКЛОНЁННЫЕ МАТЕРИАЛЫ ("<?php echo $myrefusedPromotions;?>")</strong></a>
+    <a href="//<?php echo MAIN_URL;?>/admin/viewalldraft/#bottom" class='btn btn-success btn-sm'><strong>ЧЕРНОВИК ("<?php echo $allDraft;?>")</strong></a>
     <a href="//<?php echo MAIN_URL;?>/admin/addupdpromotion/?add" class='btn btn-warning btn-sm'><strong>НАПИСАТЬ РЕКЛАМНУЮ СТАТЬЮ</strong></a>
     <?php if (userRole('Автор')):?>
         <a href="//<?php echo MAIN_URL;?>/admin/viewallauthortask/#bottom" class='btn btn-primary btn-sm'><strong>МОИ ЗАДАНИЯ ("<?php echo $myTasks;?>")</strong></a>
         <a href="//<?php echo MAIN_URL;?>/admin/viewalltask/#bottom" class='btn btn-info btn-sm'><strong>ПОЛУЧИТЬ ЗАДАНИЕ</strong></a>
-        <a href="//<?php echo MAIN_URL;?>/admin/viewalldraft/#bottom" class='btn btn-success btn-sm'><strong>ЧЕРНОВИК ("<?php echo $allDraft;?>")</strong></a>
-    <?php elseif (userRole('Автор') || userRole('Супер-автор')):?>
-        
+    <?php elseif (userRole('Автор') && userRole('Супер-автор')):?>
+        <a href="//<?php echo MAIN_URL;?>/admin/addupdpost/?add" class='btn btn-primary btn-sm'>Добавить статью</a>
+        <a href="//<?php echo MAIN_URL;?>/admin/addupdnews/?add" class='btn btn-primary btn-sm'>Добавить новость</a>
     <?php endif;?>
 
 </div>
