fef2432015d28f893d87aa632585761592f0fd3b
MrCasades <38098164+MrCasades@users.noreply.github.com>
2021-07-21 21:57:01 +0300

    do adminnews

--- admin/adminmail/viewnews/viewnews.html.php
+++ admin/adminmail/viewnews/viewnews.html.php
@@ -4,26 +4,23 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont_for_view"> 
-		
-		<div>	
-			<?php foreach ($messages as $message): ?> 
-				<div class = "post">
-				  <div class = "posttitle">
-				    <?php echo ($message['messagedate']. ' | Автор: <a href="../../../account/?id='.$message['idauthor'].'" style="color: white" >'.$message['authorname']).'</a>';?>
-					<p>E-mail: <?php echo $message['email'];?></p>
-				  </div>
-					<div align = "right"><?php echo $delAndUpdNews; ?></div>
-				   <div class = "newstext">
-					<p align = "center"><a href="../../../admin/adminmail/viewadminnews/" class="btn btn-info btn-sm">Все новости</a></p>
-					<p align = "justify"><?php echomarkdown ($message['text']);?></p>
-				   </div>	
-				 </div>
-			</div>			
-		 <?php endforeach; ?>
-		
-		</div>	
+
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<?php foreach ($messages as $message): ?> 
+	<div class="adm-news-pl m-content">
+	<?php echo $delAndUpdNews; ?>
+		<h3 class="for-info-txt"><?php htmlecho($message['messagedate']); ?>
+			<p><a href="../../../admin/adminmail/viewadminnews/" class="btn btn-info btn-sm">Все новости</a></p>
+		</h3>
+		<p><?php echomarkdown ($message['text']); ?></p>
+	</div>			
+<?php endforeach; ?>
 
 <?php 
 /*Загрузка footer*/
--- admin/adminnews/adminnews.html.php
+++ admin/adminnews/adminnews.html.php
@@ -1,11 +1,12 @@
 <div class = "main-headers">
-	<div class = "headers-places"> 
-		<div class = "main-headers-txtplace"><h2>Новости администрации</h2></div>
-	</div>
-	<div class = "main-headers-line"></div>
+    <div class = "headers-places"> 
+        <div class = "main-headers-place"><a href="./admin/adminmail/viewadminnews/">Новости администрации</a></div>
+    </div>
+    <div class = "main-headers-line"></div>
 </div>
 
 <div class="adm-news-pl m-content">
     <h3 class="for-info-txt"><?php htmlecho($messageDate); ?> | <?php htmlecho($messageTitle); ?></h3>
-    <?php echomarkdown($messageText); ?>
+	<p><?php echomarkdown (implode(' ', array_slice(explode(' ', strip_tags($messageText)), 0, 50))); ?> [...]</p>
+	<p><a href="./admin/adminmail/viewnews/?idadminnews=<?php echo $idnews; ?>"><button class="btn_2">Подробнее</button></a></p>
 </div>
