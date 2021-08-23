dd2117fa0dec9a3f19ea0ef8b0b5b5434bc95065
MrCasades <casades@list.ru>
2021-08-23 23:59:00 +0300

    new styles

--- admin/paysystemlist/paysystem.html.php
+++ admin/paysystemlist/paysystem.html.php
@@ -5,21 +5,28 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
 
-	<div class = "maincont">
-	<form action = "?<?php htmlecho ($action); ?>" method = "post">
-		<div>
-		  <label for = "paysystemname">Название платежн. системы: <input type = "text" name = "paysystemname" id = "paysystemname" value = "<?php htmlecho($paysystemname);?>"> </label>	
-		</div> 
-		<div>
-		  <input type = "hidden" name = "idpaysystem" value = "<?php htmlecho($idpaysystem);?>">
-		  <input type = "submit" value = "<?php htmlecho($button);?>" class="btn btn-primary btn-sm">
-		</div>
-	</form>	
-		<table>
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content add-main-form">
+	<p>
+		<form action = "?<?php htmlecho ($action); ?>" method = "post">
+			<div>
+				<label for = "paysystemname">Название платежн. системы: <input type = "text" name = "paysystemname" id = "paysystemname" value = "<?php htmlecho($paysystemname);?>"> </label>	
+				<input type = "hidden" name = "idpaysystem" value = "<?php htmlecho($idpaysystem);?>">
+				<input type = "submit" value = "<?php htmlecho($button);?>" class="btn_2">
+			</div>
+		</form>	
+	</p>
+	<table>
 		<tr><th>Название</th><th>Возможные действия</th></tr>
 		<?php if (empty ($paysystems))
 		 {
-			 echo '<p align = "center">Категории не добавлены</p>';
+			 echo '<p class="for-info-txt">Категории не добавлены</p>';
 		 }
 		 
 		 else
@@ -31,15 +38,15 @@ include_once MAIN_FILE . '/header.inc.php';?>
 				<td><?php htmlecho($paysystem['paysystemname']);?></td>
 				<td>
 				<input type = "hidden" name = "idpaysystem" value = "<?php echo $paysystem['id']; ?>">
-				<input type = "submit" name = "action" value = "Upd" class="btn btn-primary btn-sm">
-				<input type = "submit" name = "action" value = "Del" class="btn btn-primary btn-sm">
+				<input type = "submit" name = "action" value = "Upd" class="btn_1">
+				<input type = "submit" name = "action" value = "Del" class="btn_2">
 				</td>
 			   </div>
 		      </form>
 			</tr>
 		 <?php endforeach; ?>	
-		</table>
-	</div>	
+	</table>
+</div>
 	
 <?php 
 /*Загрузка footer*/
