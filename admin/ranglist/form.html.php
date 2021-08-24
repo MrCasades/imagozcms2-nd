a8aac5dbb03397ddc47f3f61e8fcc9b10601dd79
MrCasades <38098164+MrCasades@users.noreply.github.com>
2021-08-24 23:41:30 +0300

    new styles

--- admin/ranglist/form.html.php
+++ admin/ranglist/form.html.php
@@ -4,9 +4,15 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont">
-	<div class = "post">
+
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content add-main-form">
 	<form action = "?<?php htmlecho ($action); ?>" method = "post">
 	  <table>	
 		<tr>
@@ -24,11 +30,10 @@ include_once MAIN_FILE . '/header.inc.php';?>
 	  </table>	
 		<div>
 			<input type = "hidden" name = "id" value = "<?php htmlecho($idrang);?>">
-			<input type = "submit" value = "<?php htmlecho($button);?>" class="btn btn-primary btn-sm">
+			<input type = "submit" value = "<?php htmlecho($button);?>" class="btn_2">
 		</div>
 	</form>
-	</div>	
-	</div>
+</div>
 	
 <?php 
 /*Загрузка footer*/
--- admin/ranglist/rang.html.php
+++ admin/ranglist/rang.html.php
@@ -5,14 +5,20 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
 
-<div class = "maincont">
-	<p><a href = '?add' class="btn btn-primary btn-sm">Добавить ранг</a></p>
-	<br>
-		<table>
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content add-main-form">
+	<p><a href = '?add'><button class="btn_2">Добавить ранг</button></a></p>
+	<table>
 		<tr><th>Название</th><th>Возможные действия</th></tr>
 		<?php if (empty ($rangs))
 		 {
-			 echo '<p align = "center">Ранги не добавлены</p>';
+			 echo '<p class="for-info-txt">Ранги не добавлены</p>';
 		 }
 		 
 		 else
@@ -24,14 +30,14 @@ include_once MAIN_FILE . '/header.inc.php';?>
 				<td><?php htmlecho($rang['rangname']);?></td>
 				<td>
 				<input type = "hidden" name = "idrang" value = "<?php echo $rang['id']; ?>">
-				<input type = "submit" name = "action" value = "Upd" class="btn btn-primary btn-sm">
-				<input type = "submit" name = "action" value = "Del" class="btn btn-primary btn-sm">
+				<input type = "submit" name = "action" value = "Upd" class="btn_1">
+				<input type = "submit" name = "action" value = "Del" class="btn_2">
 				</td>
 			   </div>
 		      </form>
 			 </tr>  
 		 <?php endforeach; ?>	
-		</table>
+	</table>
 </div>
 
 <?php 
