3c1359518280ae58488d61e1f05dfeec18d06769
MrCasades <casades@list.ru>
2021-08-25 23:48:16 +0300

    new styles

--- admin/tasktypelist/form.html.php
+++ admin/tasktypelist/form.html.php
@@ -4,18 +4,23 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
-	
-	<div class = "maincont">
+
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content add-main-form">
 	<form action = "?<?php htmlecho ($action); ?>" method = "post">
 		<div>
-			<label for = "tasktypename">Название тематики: <input type = "text" name = "tasktypename" id = "tasktypename" value = "<?php htmlecho($tasktypename);?>"</label>	
-		</div> 
-		<div>
+			<label for = "tasktypename">Название тематики:</label> <input type = "text" name = "tasktypename" id = "tasktypename" value = "<?php htmlecho($tasktypename);?>">	
 			<input type = "hidden" name = "idtasktype" value = "<?php htmlecho($idtasktype);?>">
-			<input type = "submit" value = "<?php htmlecho($button);?>" class="btn btn-primary btn-sm">
+			<input type = "submit" value = "<?php htmlecho($button);?>" class="btn_2">
 		</div>
 	</form>	
-	</div>
+</div>
 
 <?php 
 /*Загрузка footer*/
--- admin/tasktypelist/tasktype.html.php
+++ admin/tasktypelist/tasktype.html.php
@@ -5,17 +5,24 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
 
-	<div class = "maincont">
-	<form action = "?<?php htmlecho ($action); ?>" method = "post">
-		<div>
-		  <label for = "tasktypename">Название рубрики: <input type = "text" name = "tasktypename" id = "tasktypename" value = "<?php htmlecho($tasktypename);?>"> </label>	
-		</div> 
-		<div>
-		  <input type = "hidden" name = "idtasktype" value = "<?php htmlecho($idtasktype);?>">
-		  <input type = "submit" value = "<?php htmlecho($button);?>" class="btn btn-primary btn-sm">
-		</div>
-	</form>	
-		<table>
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content add-main-form">
+	<p>
+		<form action = "?<?php htmlecho ($action); ?>" method = "post">
+			<div>
+				<label for = "tasktypename">Название рубрики: <input type = "text" name = "tasktypename" id = "tasktypename" value = "<?php htmlecho($tasktypename);?>"> </label>
+				<input type = "hidden" name = "idtasktype" value = "<?php htmlecho($idtasktype);?>">
+				<input type = "submit" value = "<?php htmlecho($button);?>" class="btn_2">
+			</div>
+		</form>	
+	</p>
+	<table>
 		<tr><th>Название</th><th>Возможные действия</th></tr>
 		<?php if (!isset($tasktypes))
 		 {
@@ -33,15 +40,15 @@ include_once MAIN_FILE . '/header.inc.php';?>
 				<td><?php htmlecho($tasktype['tasktypename']);?></td>
 				<td>
 				<input type = "hidden" name = "idtasktype" value = "<?php echo $tasktype['id']; ?>">
-				<input type = "submit" name = "action" value = "Upd" class="btn btn-primary btn-sm">
-				<input type = "submit" name = "action" value = "Del" class="btn btn-primary btn-sm">
+				<input type = "submit" name = "action" value = "Upd" class="btn_1">
+				<input type = "submit" name = "action" value = "Del" class="btn_2">
 				</td>
 			   </div>
 		      </form>
 			</tr>
 		 <?php endforeach; ?>	
-		</table>
-	</div>	
+	</table>
+</div>
 	
 <?php 
 /*Загрузка footer*/
