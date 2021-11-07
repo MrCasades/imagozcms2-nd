ca280be97fb37734310c262f3e76e8d37c04f692
MrCasades <casades@list.ru>
2021-11-07 12:28:53 +0300

    fix

--- admin/logpanel/logpanel.html.php
+++ admin/logpanel/logpanel.html.php
@@ -7,7 +7,7 @@
 				<input type = "hidden" name = "goto" value = "//<?php echo MAIN_URL;?>">
 				<strong><i class="fa fa-user-circle" aria-hidden="true" title="Вы вошли как"></i>: <?php echo $authorInSystem;?> | </strong> 
 				<a href="//<?php echo MAIN_URL;?>/account/?id=<?php echo $selectedAuthor;?>"><button type="button" class="btn_2"><strong><i class="fa fa-user" aria-hidden="true"></i> Профиль</strong></button></a> | 
-				<a href="//<?php echo MAIN_URL;?>/mainmessages/"><button type="button" class="btn_2"><strong><i class="fa fa-envelope" aria-hidden="true"></i> СООБЩЕНИЯ <span id = "countcolor"><?echo $unreadCount;?></span></strong></button></a>  
+				<a href="//<?php echo MAIN_URL;?>/mainmessages/"><button type="button" class="btn_2"><strong><i class="fa fa-envelope" aria-hidden="true"></i> СООБЩЕНИЯ <span id = "countcolor"><?php echo $unreadCount;?></span></strong></button></a>  
 				<?php echo $panel;?> | 
 				<button class="btn_1"><strong><i class="fa fa-sign-out" aria-hidden="true"></i> Выйти</strong></button>
 		</form> 
