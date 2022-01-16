d19f1e699843b32b10168be3eba0550def6b3bda
MrCasades <casades@list.ru>
2022-01-16 11:33:25 +0300

    new right-site-article

--- viewpost/viewpost.html.php
+++ viewpost/viewpost.html.php
@@ -104,8 +104,167 @@ include_once MAIN_FILE . '/header.inc.php';?>
 					(adsbygoogle = window.adsbygoogle || []).push({});
 				</script>
 			</div>
+
+			<div class = "main-headers">
+				<div class = "headers-places"> 
+					<div class = "main-headers-txtplace">Новости наших партнёров</div>
+				</div>
+				<div class = "main-headers-line"></div>
+			</div>
+
+			<div class="gallery-place">		
+				<div id="unit_95706"><a href="http://mirtesen.ru/" >Новости МирТесен</a></div>
+				<script type="text/javascript" charset="utf-8">
+				(function() {
+					var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
+					sc.src = '//news.mirtesen.ru/data/js/95706.js'; sc.charset = 'utf-8';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sc, s);
+				}());
+				</script>
+			</div>
+
+			<div class = "main-headers">
+				<div class = "headers-places"> 
+					<div class = "main-headers-txtplace">Комментарии (<span id="comm_count"><?php echo $countPosts; ?></span>)</div>
+				</div>
+				<div class = "main-headers-line"></div>
+			</div>
+
+			<?php echo $addComment; ?>
+			
+			<div class = "m-content comment-line"></div> 
+			<p><a name="bottom"></a></p>
+			<div id="result_form"></div>
+			<?php if (empty ($comments))
+				{
+					echo '<br/><p align="center" id="not_comment">Комментарии отсутствуют!</p>';
+				}
+					
+				else
+					
+				foreach ($comments as $comment): ?> 
+
+					<div class="comment m-content">
+						<div class="comment-person-pl">
+							<div>
+								<img src="../avatars/<?php echo $comment['avatar'];?>" alt="ava"/>
+							</div> 
+							<div>
+								<?php echo ('<a href="../account/?id='.$comment['idauthor'].'">'.$comment['authorname']).'</a>';?><br>
+								<?php echo $comment['date']; ?>
+							</div> 
+						</div>
+						<p><a name="comment-<?php echo $comment['id']; ?>"></a></p>
+						<div class="comment-text">
+							<p><?php 
+
+							//Вывод панели обновления - удаления комментария и проверка на поставленные лайки/дизлайки!
+												
+							if ($comment['islike'] == 1)
+							{
+								$likeStyle = 'fa-thumbs-up';
+								$dislikeStyle = 'fa-thumbs-o-down';
+							}
+
+							elseif ($comment['isdislike'] == 1)
+							{
+								$likeStyle = 'fa-thumbs-o-up';
+								$dislikeStyle = 'fa-thumbs-down';
+							}
+							else
+							{
+								$likeStyle = 'fa-thumbs-o-up';
+								$dislikeStyle = 'fa-thumbs-o-down';
+							}
+						
+							if (($selectedAuthor == $comment['idauthor']) || (userRole('Администратор')))
+							{
+								$updAnddel = '<form action = "?" method = "post">
+								<div>
+									<input type = "hidden" name = "id" value = "'.$comment ['id'].'">
+									<input type = "hidden" name = "idarticle" value = "'.$comment ['idarticle'].'">
+									<input type = "submit" name = "action" class="btn_2" value = "Редактировать">
+									<input type = "submit" name = "action" class="btn_1" value = "Del">
+								</div>
+								</form>';											
+							}	
+							else
+							{
+								$updAnddel = '';
+							}							 
+							
+							echo $updAnddel;?></p>
+
+							<?php echomarkdown ($comment['text']); ?>		
+						</div>
+						
+					</div>
+					<div class="comment-bottom">
+						<form class="comment-like" id = "like_form_<?php echo $comment['id'];?>">
+							<input type = "hidden" name = "idauthor" value = "<?php echo $selectedAuthor;?>">
+							<input type = "hidden" name = "idcomment" value = "<?php echo $comment['id'];?>">
+							<input type = "hidden" name = "type-like" id = "type_like_<?php echo $comment['id'];?>">
+							<button id="like_<?php echo $comment['id'];?>" class="comment-like-btn" name = "like" type="submit"><i id="lk_sign_<?php echo $comment['id'];?>" class="fa <?php echo $likeStyle;?>" aria-hidden="true"></i> <span id="likecount_<?php echo $comment['id'];?>"><?php echo $comment['likescount'];?></span></button>
+							<button id="dislike_<?php echo $comment['id'];?>" class="comment-like-btn" name ="dislike" type="submit"><i id="dlk_sign_<?php echo $comment['id'];?>" class="fa <?php echo $dislikeStyle;?>" aria-hidden="true"></i> <span id="dislikecount_<?php echo $comment['id'];?>"><?php echo $comment['dislikescount'];?></span></button>					
+						</form>
+
+						<?php 
+					/*Загрузка скрипта добавления лайков/дизлайков*/
+					include MAIN_FILE . '/includes/likescript.inc.php';?>
+
+						<div class="comment-ans">
+							<a href="#"><button class="btn_2" id = "op_form_<?php echo $comment['id'];?>"><i class="fa fa-share" aria-hidden="true"></i> Ответить</button></a> 
+							<a href="#"><button class="btn_1" id = "load_<?php echo $comment['id'];?>"><i class="fa fa-comments-o" aria-hidden="true"></i> Ответы (<span id="subcomm_count_<?php echo $comment['id']; ?>"><?php echo $comment['subcommentcount']; ?></span>)</button></a>
+						</div>
+					</div>
+					<div class = "m-content comment-line"></div>
+					<div class="m-content form-pl" id = "answ_<?php echo $comment['id'];?>" style="display: none;">
+						<?php if (isset($_SESSION['loggIn'])):?>
+							<form id="subcomm_form_<?php echo $comment['id'];?>" method = "post">
+								<textarea class = "descr mark-textarea" id = "subcomment" name = "subcomment" rows="10"></textarea>	
+								<input type = "hidden" name = "idauthor" value = "<?php echo $selectedAuthor; ?>">
+								<input type = "hidden" name = "idcomment" value = "<?php echo $comment['id']; ?>">
+								<input type = "submit" value = "Ответить" class="btn_2 addit-btn" id="add_subcomm_<?php echo $comment['id']; ?>">  
+							</form>	
+						<?php else:?>
+							<div class="for-info-txt">
+								<a href="../admin/registration/?log">Авторизируйтесь</a> в системе или 
+								<a href="../admin/registration/?reg">зарегестрируйтесь</a> для того, чтобы ответиь на комментарий!
+							</div>
+						<?php endif;?>
+					</div> 
+					<div class="m-content" id="hide_open_pl_<?php echo $comment['id']; ?>" style="display: none;"><a href="#" id="subcomment_hide_<?php echo $comment['id']; ?>">Скрыть</a> <a href="../viewwallpost/?id=<?php echo $comment['id']; ?>&typeart=post&idart=<?php echo $idPost; ?>">Все ответы</a></div>
+					<div id="result_form_<?php echo $comment['id']; ?>"></div>
+					<div id="subcomments_<?php echo $comment['id']; ?>"></div>
+					
+					<?php 
+					/*Загрузка скрипта получения субкомментов в шаблон*/
+					include MAIN_FILE . '/includes/subcommentloadscript.inc.php';?>
+		
+				<?php endforeach; ?>
+
+			<div class="page-output">	
+			<?php
+				/*Постраничный вывод информации*/
+				for ($i = 1; $i <= $pagesCount; $i++) 
+				{
+					// если текущая старница
+					if($i == $page)
+					{
+						echo "<a href='./viewpost/?id=".$idPost."&page=$i'><button class='btn_2'>$i</button></a> ";
+					} 
+					else 
+					{
+						echo "<a href='./viewpost/?id=".$idPost."&page=$i'><button class='btn_1'>$i</button></a> ";
+					}
+				}?>
+			</div>
 		</div>
-		<div class="right-side">			
+		<div class="right-side">
+			<?php
+			/*Загрузка компонента последних новостей*/
+			include_once MAIN_FILE . '/newsblockinrightside/newsblockinrightside.inc.php';?>
+
 			<div class = "main-headers">
 				<div class = "headers-places"> 
 					<div class = "main-headers-txtplace">Случайные статьи рубрики</div>
@@ -140,163 +299,7 @@ include_once MAIN_FILE . '/header.inc.php';?>
 				</div>
 			</div>
 		</div>
-	</div>
-
-		<div class = "main-headers">
-            <div class = "headers-places"> 
-                <div class = "main-headers-txtplace">Новости наших партнёров</div>
-            </div>
-            <div class = "main-headers-line"></div>
-        </div>
-
-        <div class="gallery-place">		
-			<div id="unit_95706"><a href="http://mirtesen.ru/" >Новости МирТесен</a></div>
-			<script type="text/javascript" charset="utf-8">
-			(function() {
-				var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
-				sc.src = '//news.mirtesen.ru/data/js/95706.js'; sc.charset = 'utf-8';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sc, s);
-			}());
-			</script>
-		</div>
-
-		<div class = "main-headers">
-            <div class = "headers-places"> 
-                <div class = "main-headers-txtplace">Комментарии (<span id="comm_count"><?php echo $countPosts; ?></span>)</div>
-            </div>
-            <div class = "main-headers-line"></div>
-        </div>
-
-		<?php echo $addComment; ?>
-		
-		<div class = "m-content comment-line"></div> 
-		<p><a name="bottom"></a></p>
-		<div id="result_form"></div>
-		<?php if (empty ($comments))
-			{
-				echo '<br/><p align="center" id="not_comment">Комментарии отсутствуют!</p>';
-			}
-				
-			else
-				
-			foreach ($comments as $comment): ?> 
-
-				<div class="comment m-content">
-					<div class="comment-person-pl">
-						<div>
-							<img src="../avatars/<?php echo $comment['avatar'];?>" alt="ava"/>
-						</div> 
-						<div>
-							<?php echo ('<a href="../account/?id='.$comment['idauthor'].'">'.$comment['authorname']).'</a>';?><br>
-							<?php echo $comment['date']; ?>
-						</div> 
-					</div>
-					<p><a name="comment-<?php echo $comment['id']; ?>"></a></p>
-					<div class="comment-text">
-						<p><?php 
-
-						//Вывод панели обновления - удаления комментария и проверка на поставленные лайки/дизлайки!
-											
-						if ($comment['islike'] == 1)
-						{
-							$likeStyle = 'fa-thumbs-up';
-							$dislikeStyle = 'fa-thumbs-o-down';
-						}
-
-						elseif ($comment['isdislike'] == 1)
-						{
-							$likeStyle = 'fa-thumbs-o-up';
-							$dislikeStyle = 'fa-thumbs-down';
-						}
-						else
-						{
-							$likeStyle = 'fa-thumbs-o-up';
-							$dislikeStyle = 'fa-thumbs-o-down';
-						}
-					
-						if (($selectedAuthor == $comment['idauthor']) || (userRole('Администратор')))
-						{
-							$updAnddel = '<form action = "?" method = "post">
-							<div>
-								<input type = "hidden" name = "id" value = "'.$comment ['id'].'">
-								<input type = "hidden" name = "idarticle" value = "'.$comment ['idarticle'].'">
-								<input type = "submit" name = "action" class="btn_2" value = "Редактировать">
-								<input type = "submit" name = "action" class="btn_1" value = "Del">
-							</div>
-							</form>';											
-						}	
-						else
-						{
-							$updAnddel = '';
-						}							 
-						
-						echo $updAnddel;?></p>
-
-						<?php echomarkdown ($comment['text']); ?>		
-					</div>
-					
-				</div>
-				<div class="comment-bottom">
-					<form class="comment-like" id = "like_form_<?php echo $comment['id'];?>">
-						<input type = "hidden" name = "idauthor" value = "<?php echo $selectedAuthor;?>">
-						<input type = "hidden" name = "idcomment" value = "<?php echo $comment['id'];?>">
-						<input type = "hidden" name = "type-like" id = "type_like_<?php echo $comment['id'];?>">
-						<button id="like_<?php echo $comment['id'];?>" class="comment-like-btn" name = "like" type="submit"><i id="lk_sign_<?php echo $comment['id'];?>" class="fa <?php echo $likeStyle;?>" aria-hidden="true"></i> <span id="likecount_<?php echo $comment['id'];?>"><?php echo $comment['likescount'];?></span></button>
-						<button id="dislike_<?php echo $comment['id'];?>" class="comment-like-btn" name ="dislike" type="submit"><i id="dlk_sign_<?php echo $comment['id'];?>" class="fa <?php echo $dislikeStyle;?>" aria-hidden="true"></i> <span id="dislikecount_<?php echo $comment['id'];?>"><?php echo $comment['dislikescount'];?></span></button>					
-					</form>
-
-					<?php 
-				/*Загрузка скрипта добавления лайков/дизлайков*/
-				include MAIN_FILE . '/includes/likescript.inc.php';?>
-
-					<div class="comment-ans">
-						<a href="#"><button class="btn_2" id = "op_form_<?php echo $comment['id'];?>"><i class="fa fa-share" aria-hidden="true"></i> Ответить</button></a> 
-						<a href="#"><button class="btn_1" id = "load_<?php echo $comment['id'];?>"><i class="fa fa-comments-o" aria-hidden="true"></i> Ответы (<span id="subcomm_count_<?php echo $comment['id']; ?>"><?php echo $comment['subcommentcount']; ?></span>)</button></a>
-					</div>
-				</div>
-				<div class = "m-content comment-line"></div>
-				<div class="m-content form-pl" id = "answ_<?php echo $comment['id'];?>" style="display: none;">
-					<?php if (isset($_SESSION['loggIn'])):?>
-						<form id="subcomm_form_<?php echo $comment['id'];?>" method = "post">
-							<textarea class = "descr mark-textarea" id = "subcomment" name = "subcomment" rows="10"></textarea>	
-							<input type = "hidden" name = "idauthor" value = "<?php echo $selectedAuthor; ?>">
-							<input type = "hidden" name = "idcomment" value = "<?php echo $comment['id']; ?>">
-							<input type = "submit" value = "Ответить" class="btn_2 addit-btn" id="add_subcomm_<?php echo $comment['id']; ?>">  
-						</form>	
-					<?php else:?>
-						<div class="for-info-txt">
-							<a href="../admin/registration/?log">Авторизируйтесь</a> в системе или 
-							<a href="../admin/registration/?reg">зарегестрируйтесь</a> для того, чтобы ответиь на комментарий!
-						</div>
-					<?php endif;?>
-				</div> 
-				<div class="m-content" id="hide_open_pl_<?php echo $comment['id']; ?>" style="display: none;"><a href="#" id="subcomment_hide_<?php echo $comment['id']; ?>">Скрыть</a> <a href="../viewwallpost/?id=<?php echo $comment['id']; ?>&typeart=post&idart=<?php echo $idPost; ?>">Все ответы</a></div>
-				<div id="result_form_<?php echo $comment['id']; ?>"></div>
-				<div id="subcomments_<?php echo $comment['id']; ?>"></div>
-				
-				<?php 
-				/*Загрузка скрипта получения субкомментов в шаблон*/
-				include MAIN_FILE . '/includes/subcommentloadscript.inc.php';?>
-	
-			<?php endforeach; ?>
-
-		<div class="page-output">	
-		 <?php
-			/*Постраничный вывод информации*/
-			for ($i = 1; $i <= $pagesCount; $i++) 
-			{
-				// если текущая старница
-				if($i == $page)
-				{
-					echo "<a href='./viewpost/?id=".$idPost."&page=$i'><button class='btn_2'>$i</button></a> ";
-				} 
-				else 
-				{
-					echo "<a href='./viewpost/?id=".$idPost."&page=$i'><button class='btn_1'>$i</button></a> ";
-				}
-			}?>
-</div>
-
+	</div>	
 </article>
 
 <?php 
