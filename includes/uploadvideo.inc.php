9011f551a132162946eb07000e629cb4b5e7c467
MrCasades <casades@list.ru>
2022-04-17 13:13:24 +0300

    add video

--- includes/uploadvideo.inc.php
+++ includes/uploadvideo.inc.php
@@ -4,30 +4,36 @@
 	
 	/*Извлечение расширения файла*/
 	
-if (preg_match ("/\.(mp4|MP4)$/", $_FILES['upload']['type']))
+$ext = pathinfo($_FILES['uploadvideo']['name'], PATHINFO_EXTENSION);
+
+if($ext  == 'mp4')
 {
-	$ext = '.mp4';
-}
-	
-elseif (preg_match ('/^video\/p?webm$/i', $_FILES['upload']['type']))
-{
-	$ext = '.webm';
-}
-	
-elseif (preg_match ('/^video\/p?ogv$/i', $_FILES['upload']['type']))
-{
-	$ext = '.ogv';
-}
-	
-else	
-{
-	$ext = '.unk';
-}
+
+	// if (preg_match ('/^.*\.(mp4)$/i', $_FILES['uploadvideo']['type']))
+	// {
+	// 	$ext = '.mp4';
+	// }
 		
-$VideoFileName = $fileNameVideoScript.$ext;//присвоение имени файла
-$videoFilePath = MAIN_FILE . $filePathVideoScript . $VideoFileName;//путь загрузки
+	// elseif (preg_match ('/^video\/p?webm$/i', $_FILES['uploadvideo']['type']))
+	// {
+	// 	$ext = '.webm';
+	// }
 		
-if (!is_uploaded_file($_FILES['uploadvideo']['tmp_name']) or !copy($_FILES['uploadvideo']['tmp_name'], $videoFilePath))
-{
-	$VideoFileName = '';
+	// elseif (preg_match ('/^video\/p?ogv$/i', $_FILES['uploadvideo']['type']))
+	// {
+	// 	$ext = '.ogv';
+	// }
+		
+	// else	
+	// {
+	// 	$ext = '.unk';
+	// }
+			
+	$VideoFileName = $fileNameVideoScript.'.'.$ext;//присвоение имени файла
+	$videoFilePath = MAIN_FILE . $filePathVideoScript . $VideoFileName;//путь загрузки
+			
+	if (!is_uploaded_file($_FILES['uploadvideo']['tmp_name']) or !copy($_FILES['uploadvideo']['tmp_name'], $videoFilePath))
+	{
+		$VideoFileName = '';
+	}
 }
