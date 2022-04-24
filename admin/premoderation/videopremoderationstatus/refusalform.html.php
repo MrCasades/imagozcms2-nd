a3920f82b2a45e92d14944e84620c0bb184066c3
MrCasades <casades@list.ru>
2022-04-24 16:11:32 +0300

    add video

---
+++ admin/premoderation/videopremoderationstatus/refusalform.html.php
@@ -0,0 +1,30 @@
+<?php 
+/*Загрузка функций в шаблон*/
+include_once MAIN_FILE . '/includes/func.inc.php';
+
+/*Загрузка header*/
+include_once MAIN_FILE . '/header.inc.php';?>
+	
+<div class = "main-headers">
+    <div class = "headers-places"> 
+        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
+    </div>
+    <div class = "main-headers-line"></div>
+</div>
+
+<div class="m-content task-pl task-txt">
+	<p class="for-info-txt"><?php htmlecho($premodYes); ?> "<?php htmlecho($posttitle); ?>"?</p>
+	<form action = "?<?php htmlecho($action); ?> " method = "post">		
+		<label for = "reasonrefusal" class="for-info-txt">Причина отказа </label>
+		<textarea class = "mark-textarea" id = "reasonrefusal" name = "reasonrefusal" rows="10"><?php htmlecho($reasonrefusal);?></textarea>	
+		<p>
+			<input type = "hidden" name = "id" value = "<?php htmlecho($id); ?>">
+		  	<input type = "submit" name = "delete" class="btn_1 addit-btn" value = "<?php htmlecho($button); ?>">
+		  	<a href="#" onclick="history.back();"><button class="btn_2 addit-btn" type="button">Назад</button></a>
+		</p>
+	</form> 
+</div>
+
+<?php 
+/*Загрузка footer*/
+include_once MAIN_FILE . '/footer.inc.php';?>
