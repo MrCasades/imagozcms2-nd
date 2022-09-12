8f740f70332adfcb579d3dca2138e8c76a90ef80
MrCasades <casades@list.ru>
2022-09-12 18:41:35 +0300

    start new design

--- account/account.html.php
+++ account/account.html.php
@@ -6,12 +6,13 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 include_once MAIN_FILE . '/header.inc.php';?>
 
 <main>
-    <div class = "main-headers">
-        <div class = "headers-places"> 
-            <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?></h1></div>
-        </div>
-        <div class = "main-headers-line"></div>   
-    </div>
+	<div class = "main-headers">
+		<div class = "main-headers-circle"></div>
+		<div class = "main-headers-content">
+				<h2><?php htmlecho ($headMain); ?></h2>
+			<div class = "main-headers-line"></div>
+		</div>
+	</div>
 
 	<div class="acc-m m-content">
         <div class="ava-pl">
