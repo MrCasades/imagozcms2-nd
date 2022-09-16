b4e2bf3532a333a90d45a10643081f2ffab7249a
MrCasades <casades@list.ru>
2022-09-17 00:28:23 +0300

    fix headers and styles

--- admin/premoderation/viewpremodnews/viewpremodnews.html.php
+++ admin/premoderation/viewpremodnews/viewpremodnews.html.php
@@ -10,10 +10,11 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 include_once MAIN_FILE . '/header.inc.php';?>
 
 <div class = "main-headers">
-    <div class = "headers-places"> 
-        <div class = "main-headers-txtplace"><h1><a href="#" onclick="history.back();">Назад</a></h1></div>
-    </div>
-    <div class = "main-headers-line"></div>
+	<div class = "main-headers-circle"></div>
+	<div class = "main-headers-content">
+    	<h2><a href="#" onclick="history.back();">Назад</a></h2>
+		<div class = "main-headers-line"></div>
+	</div>
 </div>
 
 <article>
