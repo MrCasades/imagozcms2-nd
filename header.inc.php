28899786e75248e6c0d99b1a96718b85f660daa1
MrCasades <casades@list.ru>
2022-12-12 00:53:38 +0300

    puls link in header

--- header.inc.php
+++ header.inc.php
@@ -87,6 +87,7 @@
 			<div class="header-social-net-pl">
 				<a href = "https://vk.com/imagoz" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/vк1.png';?>" alt="Наша группа VK" title="Наша группа VK"/></a>
 				<a href = "https://zen.yandex.ru/imagoz" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/zen2.png';?>" alt="Наш Дзен-канал" title="Наш Дзен-канал"/></a>
+				<a href = "https://pulse.mail.ru/imagoz-igry-i-tehnologii/" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/mail3.png';?>" alt="Мы на Пульсе!" title="Мы на Пульсе!"/></a>
 			</div>
 			<div class="login-logout-btn-pl">
 				<?php if (!isset($_SESSION['loggIn'])):?>
