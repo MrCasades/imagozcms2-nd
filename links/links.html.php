4aec0ec26dfca4f5f3cdbb59e3c8dbc06cc0f26f
MrCasades <casades@list.ru>
2023-03-04 17:00:01 +0300

    new site description

---
+++ links/links.html.php
@@ -0,0 +1,25 @@
+<?php 
+/*Загрузка функций в шаблон*/
+include_once MAIN_FILE . '/includes/func.inc.php';
+
+/*Загрузка header*/
+include_once MAIN_FILE . '/header.inc.php';?>
+
+<div class = "main-headers">
+	<div class = "main-headers-circle"></div>
+	<div class = "main-headers-content">
+    	<h2><?php htmlecho ($headMain); ?></h2>
+		<div class = "main-headers-line"></div>
+	</div>
+</div>
+
+<div class="m-content">
+	<p class="for-info-logo"><img src="<?php echo '//'.MAIN_URL;?>/decoration/logo2.png" alt="imagoz.ru | Сотрудничество"></p>
+	<div class="for-info-font">
+		<?php echo $links; ?>
+	</div>
+</div>
+	
+<?php 
+/*Загрузка footer*/
+include_once MAIN_FILE . '/footer.inc.php';?>	
