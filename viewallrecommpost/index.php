20a952bee52cf1ef23de3baa93b5e65b7c0eab34
MrCasades <casades@list.ru>
2023-06-27 23:37:44 +0300

    add block settings

--- viewallrecommpost/index.php
+++ viewallrecommpost/index.php
@@ -5,21 +5,15 @@ include_once '../includes/path.inc.php';
 /*Загрузка общих переменных*/
 include_once MAIN_FILE . '/includes/commonvar.inc.php';
 
-$title = 'Каталог статей | imagoz.ru';//Данные тега <title>
-$headMain = 'Все рекомендованные статьи';
-$robots = 'noindex, follow';
-$descr = 'В данном разделе размещаются список всех статей портала';
-$breadPart1 = '<a href="//'.MAIN_URL.'">Главная страница</a> >> '; //Для хлебных крошек
-$breadPart2 = '<a href="//'.MAIN_URL.'/viewallrecommpost/">Все рекомендации</a> ';//Для хлебных крошек
-
 /*Загрузка функций для формы входа*/
 require_once MAIN_FILE . '/includes/access.inc.php';
 
+/*Загрузка настроек раздела*/
+$blockFolder = 'viewallrecommpost';
+include_once MAIN_FILE . '/includes/blocksettings/blockset.inc.php';
+
 /*Определение нахождения пользователя в системе*/
-if (loggedIn())
-{
-	/*Если loggedIn = TRUE, выводится имя пользователя иначе меню авторизации*/
-}
+loggedIn();
 
 /*Подключение к базе данных*/
 include MAIN_FILE . '/includes/db.inc.php';
