c660af88c329ae818e58f1b8fe0ac9a8c2c30206
MrCasades <casades@list.ru>
2023-07-31 00:44:06 +0300

    make blogs

---
+++ admin/premoderation/blogpremoderation/blogpremoderation.html.php
@@ -0,0 +1,51 @@
+<?php 
+/*Загрузка функций в шаблон*/
+include_once MAIN_FILE . '/includes/func.inc.php';
+
+/*Загрузка header*/
+include_once MAIN_FILE . '/header.inc.php';?>
+
+<div class = "main-headers">
+	<div class = "main-headers-circle"></div>
+	<div class = "main-headers-content">
+    	<h2><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h2>
+		<div class = "main-headers-line"></div>
+	</div>
+</div>
+
+<div class = "m-content">
+	<table class = "for-tables-premod">
+		  <tr>
+				<th>#id</th>
+				<th>Дата публикации</th>
+				<th>Заголовок</th>	
+				<th>Блог</th>
+				<th>Премодерация блога</th>
+				<th>Автор</th>
+				<th>E-mail</th>				
+		  </tr> 
+		  
+		   <?php if (empty ($blogpubs))
+		 {
+			 echo '<p class = "for-info-txt">Материалы для премодерации отсутствуют</p>';
+		 }
+
+		 else
+			 
+		 foreach ($blogpubs as $blogpub): ?> 
+		  <tr>
+				<td><?php echo '# '.$blogpub['id'];?></td>
+				<td><?php echo $blogpub['date'];?></td>
+				<td><a href="../../admin/premoderation/viewpremodblogpub/?blogpub=<?php echo $blogpub['id'];?>"><?php echo $blogpub['pubtitle'];?></a></td>
+				<td><a href="//<?php echo MAIN_URL ;?>/blog/?id=<?php echo $blogpub['idblog'];?>" target="blank_"><?php echo $blogpub['blogtitle'];?></a></td>
+				<td><?php echo $blogpub['blogpremoderation'];?></td>
+				<td><?php echo $blogpub['authorname'];?></td>
+				<td><?php echo $blogpub['email'];?></td>
+		  </tr> 				
+		 <?php endforeach; ?> 
+	</table>
+</div>	
+		
+<?php 
+/*Загрузка footer*/
+include_once MAIN_FILE . '/footer.inc.php';?>	
---
+++ admin/premoderation/blogpremoderation/index.php
@@ -0,0 +1,55 @@
+<?php
+/*Загрузка главного пути*/
+include_once '../../../includes/path.inc.php';
+
+$title = 'Премодерация блогов';//Данные тега <title>
+$headMain = 'Блоги в премодерации';
+$robots = 'noindex, nofollow';
+$descr = 'Вданном разделе выводятся материалы для премодерации';
+
+/*Загрузка функций для формы входа*/
+require_once MAIN_FILE . '/includes/access.inc.php';
+
+/*Загрузка формы входа*/
+if (!loggedIn())
+{
+	include '../login.html.php';
+	exit();
+}
+
+/*Загрузка сообщения об ошибке входа*/
+if (!userRole('Администратор'))
+{
+	$error = 'Доступ запрещен';
+	include '../accessfail.html.php';
+	exit();
+}
+
+/*Подключение к базе данных*/
+include MAIN_FILE . '/includes/db.inc.php';
+
+/*Вывод новостей*/
+/*Команда SELECT*/
+
+try
+{
+	$sql = 'SELECT newsblock.id, newstitle, newsdate, authorname, email FROM newsblock INNER JOIN author 
+			ON idauthor = author.id WHERE premoderation = "NO" AND refused = "NO" AND draft = "NO" LIMIT 20';//Вверху самое последнее значение
+	$result = $pdo->query($sql);
+}
+
+catch (PDOException $e)
+{
+	$error = 'Ошибка вывода новостей в премодерации';
+	include MAIN_FILE . '/includes/error.inc.php';
+}
+
+/*Вывод результата в шаблон*/
+foreach ($result as $row)
+{
+	$newsIn[] =  array ('id' => $row['id'], 'newstitle' =>  $row['newstitle'], 'newsdate' =>  $row['newsdate'], 
+						'authorname' =>  $row['authorname'], 'email' =>  $row['email']);
+}
+
+include 'blogpremoderation.html.php';
+exit();
