1c8e2b9749b6dd1d028a6f604f492e4d4339d848
MrCasades <casades@list.ru>
2023-08-12 12:52:24 +0300

    make blogs

--- blogs/blogs.html.php
+++ blogs/blogs.html.php
@@ -6,8 +6,10 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 include_once MAIN_FILE . '/header.inc.php';?>
 
 	<div class = "main-headers">
+		<div class = "main-headers-circle"></div>
 		<div class = "main-headers-content">
 			<h2><?php htmlecho ($headMain); ?></h2>
+			<div class = "main-headers-line"></div>				
 		</div>
 	</div>
 
