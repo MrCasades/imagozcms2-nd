f6ff2c938021fa1ada1033b38425f3c6c05396f1
MrCasades <casades@list.ru>
2023-09-23 00:14:41 +0300

    new search

--- searchpost/searchpost.html.php
+++ searchpost/searchpost.html.php
@@ -12,8 +12,8 @@ include_once MAIN_FILE . '/includes/func.inc.php';?>
 
 	<?php foreach ($posts as $post): ?>
 
-		<a href="../viewpost/?id=<?php htmlecho ($post['id']); ?>"><h3><?php htmlecho ($post['posttitle']); ?></h3></a>
-		<p>Рубрика: <a href="../viewcategory/?id=<?php echo $post['categoryid']; ?>"><?php echo $post['categoryname'];?></a></p>
+		<a href="//<?php htmlecho (MAIN_URL); ?>/viewpost/?id=<?php htmlecho ($post['id']); ?>"><h3><?php htmlecho ($post['posttitle']); ?></h3></a>
+		<p>Рубрика: <a href="//<?php htmlecho (MAIN_URL); ?>/viewcategory/?id=<?php echo $post['categoryid']; ?>"><?php echo $post['categoryname'];?></a></p>
 		<p><?php echomarkdown (implode(' ', array_slice(explode(' ', strip_tags($post['text'])), 0, 50))); ?> [...]</p>
 	<?php endforeach; ?>
 
@@ -21,8 +21,8 @@ include_once MAIN_FILE . '/includes/func.inc.php';?>
 
 	<?php foreach ($newsIn as $news): ?>
 
-		<a href="../viewnews/?id=<?php htmlecho ($news['id']); ?>"><h3><?php htmlecho ($news['newstitle']); ?></h3></a>
-		<p>Рубрика: <a href="../viewcategory/?id=<?php echo $news['categoryid']; ?>"><?php echo $news['categoryname'];?></a></p>
+		<a href="//<?php htmlecho (MAIN_URL); ?>/viewnews/?id=<?php htmlecho ($news['id']); ?>"><h3><?php htmlecho ($news['newstitle']); ?></h3></a>
+		<p>Рубрика: <a href="//<?php htmlecho (MAIN_URL); ?>/viewcategory/?id=<?php echo $news['categoryid']; ?>"><?php echo $news['categoryname'];?></a></p>
 		<p><?php echomarkdown (implode(' ', array_slice(explode(' ', strip_tags($news['textnews'])), 0, 50))); ?> [...]</p>
 	<?php endforeach; ?>
 
@@ -30,8 +30,8 @@ include_once MAIN_FILE . '/includes/func.inc.php';?>
 
 	<?php foreach ($promotions as $promotion): ?>
 
-		<a href="../viewpromotion/?id=<?php htmlecho ($promotion['id']); ?>"><h3><?php htmlecho ($promotion['promotiontitle']); ?></h3></a>
-		<p>Рубрика: <a href="../viewcategory/?id=<?php echo $promotion['categoryid']; ?>"><?php echo $promotion['categoryname'];?></a></p>
+		<a href="//<?php htmlecho (MAIN_URL); ?>/viewpromotion/?id=<?php htmlecho ($promotion['id']); ?>"><h3><?php htmlecho ($promotion['promotiontitle']); ?></h3></a>
+		<p>Рубрика: <a href="//<?php htmlecho (MAIN_URL); ?>/viewcategory/?id=<?php echo $promotion['categoryid']; ?>"><?php echo $promotion['categoryname'];?></a></p>
 		<p><?php echomarkdown (implode(' ', array_slice(explode(' ', strip_tags($promotion['text'])), 0, 50))); ?> [...]</p>
 	<?php endforeach; ?>
 
@@ -47,7 +47,7 @@ include_once MAIN_FILE . '/includes/func.inc.php';?>
 		
 			<div class = "newsblock m-content">			 
 				<?php foreach ($pubs as $pub): ?> 
-				<a href = "../blog/publication?id=<?php htmlecho ($pub['id']); ?>" class = "post-place-1" style="background-image: url(../images/<?php echo $pub['imghead']; ?>)">
+				<a href = "//<?php htmlecho (MAIN_URL); ?>/blog/publication?id=<?php htmlecho ($pub['id']); ?>" class = "post-place-1" style="background-image: url(../images/<?php echo $pub['imghead']; ?>)">
 					<div class = "post-top-1">
 						<p><?php echo date("Y.m.d H:i", strtotime($pub['date'])); ?></p>
 						<span class="post-rubrics"><?php htmlecho ($pub['categoryname']); ?></span>
@@ -69,7 +69,7 @@ include_once MAIN_FILE . '/includes/func.inc.php';?>
 
 	<div class = "main-post m-content">
 		<?php foreach ($blogs as $blog): ?>
-			<a href="../blog/?id=<?php htmlecho ($blog['id']); ?>" class = "post-place-2" style="background-image: url(//<?php htmlecho (MAIN_URL); ?>/blog/avatars/<?php echo $blog['avatar']; ?>)">				
+			<a href="//<?php htmlecho (MAIN_URL); ?>/blog/?id=<?php htmlecho ($blog['id']); ?>" class = "post-place-2" style="background-image: url(//<?php htmlecho (MAIN_URL); ?>/blog/avatars/<?php echo $blog['avatar']; ?>)">				
 				<div class = "post-bottom-1"><?php htmlecho ((implode(' ', array_slice(explode(' ', strip_tags($blog['title'])), 0, 7)))); ?>...</div>
 			</a>
 				
