004cf7f7273f35173586e08dc670cb3c91f3cb09
MrCasades <casades@list.ru>
2023-10-20 00:57:05 +0300

    new pahination

--- viewmetavideo/index.php
+++ viewmetavideo/index.php
@@ -83,6 +83,10 @@ if (isset ($_GET['metaid']))
 	
 	$countPosts = $row["all_articles"];
 	$pagesCount = ceil($countPosts / $onPage);
+	$previousPage = $page - 1;
+	$nextPage = $page + 1;
+	$secondLast = $pagesCount -1;
+	$additData = '&metaid='.$idMeta;
 		
 	include 'metavideo.html.php';
 	exit();		
