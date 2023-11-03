1a60e73f00d8976e417f978b03485593e76fcc43
MrCasades <casades@list.ru>
2023-11-04 02:54:44 +0300

    set block button

--- pagesettings/settingbuttons.php
+++ pagesettings/settingbuttons.php
@@ -1,7 +1,8 @@
 <?php
-$mainpageSet = '';
 
 if (userRole('Администратор'))	
 {
-    $mainpageSet = "<a href='//".MAIN_URL."/pagesettings/mainpagesetting/'><button class='btn_1 addit-btn'>Настроить</button></a>";
+    //$mainpageSet = "<a href='//".MAIN_URL."/pagesettings/mainpagesetting/'><button class='btn_1 addit-btn'>Настроить</button></a>";
+
+    $pageSetButton = "<a href='//".MAIN_URL."/pagesettings/".$blockFolder."setting/'><button class='btn_1 addit-btn'>Настроить</button></a>";
 }
