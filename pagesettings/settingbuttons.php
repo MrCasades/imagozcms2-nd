96d7efac79be1f51bf2a1fa3a3a363e00a8a55ed
MrCasades <casades@list.ru>
2023-11-10 01:06:50 +0300

    setting buttons

--- pagesettings/settingbuttons.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (userRole('Администратор'))	
-{
-    //$mainpageSet = "<a href='//".MAIN_URL."/pagesettings/mainpagesetting/'><button class='btn_1 addit-btn'>Настроить</button></a>";
-
-    $pageSetButton = "<a href='//".MAIN_URL."/pagesettings/".$blockFolder."setting/'><button class='btn_1 addit-btn'>Настроить</button></a>";
-}
