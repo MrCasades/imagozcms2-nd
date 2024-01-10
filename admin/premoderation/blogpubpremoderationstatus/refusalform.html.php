275f31b84e0695a1545dee16dfc89171796186f4
MrCasades <casades@list.ru>
2024-01-11 00:54:20 +0300

    fix headers

--- admin/premoderation/blogpubpremoderationstatus/refusalform.html.php
+++ admin/premoderation/blogpubpremoderationstatus/refusalform.html.php
@@ -5,7 +5,7 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 /*Загрузка header*/
 include_once MAIN_FILE . '/header.inc.php';?>
 	
-	<div class = "main-headers">
+<div class = "main-headers">
 	<div class = "main-headers-circle"></div>
 	<div class = "main-headers-content">
     	<h2><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h2>
--- admin/refused/refusedpromotion.html.php
+++ admin/refused/refusedpromotion.html.php
@@ -6,10 +6,9 @@ include_once MAIN_FILE . '/includes/func.inc.php';
 include_once MAIN_FILE . '/header.inc.php';?>
 
 <div class = "main-headers">
-    <div class = "headers-places"> 
-        <div class = "main-headers-txtplace"><h1><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h1></div>
-    </div>
-    <div class = "main-headers-line"></div>
+	<div class = "main-headers-content">
+    	<h2><?php htmlecho ($headMain); ?> | <a href="#" onclick="history.back();">Назад</a></h2>
+	</div>
 </div>
 
 <div class="m-content">
