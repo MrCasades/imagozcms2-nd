6ca2b3f8959c92b9f0124580b3efba3959edc122
MrCasades <casades@list.ru>
2024-03-12 00:02:59 +0300

    auto upd rss

---
+++ admin/updrss.inc.php
@@ -0,0 +1,8 @@
+<div id="result"></div>
+<script>
+    $(document).ready(function() {
+        $('#result').load('//'+document.location.host + '/rsssitemap/');
+        $('#result').load('//'+document.location.host + '/rsssmi/');
+        $('#result').load('//'+document.location.host + '/rssvk/');
+    })
+</script>
