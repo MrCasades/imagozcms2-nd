a78a4983b43ceaa8055205bcc4b2f3044bbba75d
MrCasades <casades@list.ru>
2024-05-13 00:52:44 +0300

    add link block

--- header.inc.php
+++ header.inc.php
@@ -115,12 +115,9 @@
 			</div>
 			
 			<?php if ($data_common['difflinks'] == "on") :?>
-				<div class="header-social-net-pl">
-					<a href = "https://vk.com/imagoz" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/vк1.png';?>" alt="Наша группа VK" title="Наша группа VK"/></a>
-					<a href = "https://zen.yandex.ru/imagoz" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/zen2.png';?>" alt="Наш Дзен-канал" title="Наш Дзен-канал"/></a>
-					<!-- <a href = "https://pulse.mail.ru/imagoz-igry-i-tehnologii/" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/mail3.png';?>" alt="Мы на Пульсе!" title="Мы на Пульсе!"/></a> -->
-					<a href = "https://www.youtube.com/channel/UCYAlQfGJQC4de8gEmZwF9Yg" target="_blank"><img src="<?php echo '//'.MAIN_URL.'/decoration/youtube.png';?>" alt="Мы на YouTube!" title="Мы на YouTube!"/></a>
-				</div>
+				<?php 
+						/*Загрузка меню авторизации*/
+						include_once MAIN_FILE . '/includes/diffblocks/difflinksheader.inc.html.php';?>
 			<?php endif;?>
 
 			<div class="login-logout-btn-pl">
