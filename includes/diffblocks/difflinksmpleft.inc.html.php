3fda6e2ac317c8b433ecc247145f4e28b1288a9c
MrCasades <casades@list.ru>
2024-05-16 01:06:39 +0300

    new mp settings

---
+++ includes/diffblocks/difflinksmpleft.inc.html.php
@@ -0,0 +1,49 @@
+<div class="zen-c-m">
+	<div class = "main-headers">
+		<div class = "main-headers-content">
+			<h2 class="no-link-header">Наш Дзен-канал</h2>
+			<div class = "main-headers-line"></div>				
+		</div>
+	</div>
+	<div class="zen-link-m">
+		<a href="https://zen.yandex.ru/imagoz"><img src="./zen-icon.png" alt="Наш Дзен-канал" title="zen.yandex.ru/imagoz"></a>
+	</div>
+</div>
+<div class="zen-c-m">
+	<div class = "main-headers">
+		<div class = "main-headers-content">
+			<h2 class="no-link-header">Наш YouTube-канал</h2>
+			<div class = "main-headers-line"></div>				
+	    </div>
+	</div>
+	<div class="zen-link-m">
+		<a href="https://www.youtube.com/channel/UCYAlQfGJQC4de8gEmZwF9Yg"><img src="<?php echo '//'.MAIN_URL.'/decoration/youtubem.png';?>" alt="Мы на YouTube!" title="Мы на YouTube!"/></a>
+	</div>
+</div>
+<div class="vk-m">
+	<div class = "main-headers">
+		<div class = "main-headers-content">
+			<h2 class="no-link-header">Мы ВКонтакте</h2>
+			<div class = "main-headers-line"></div>				
+		</div>
+	</div>
+	<div class="scr-cont">
+		<script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
+
+			<!-- VK Widget -->
+			<div id="vk_groups"></div>
+			<script type="text/javascript">
+				VK.Widgets.Group("vk_groups", {mode: 3, width: 170, no_cover: 1}, 54027668);
+			</script>					
+	</div>
+</div>
+<br>
+<!-- Yandex.RTB R-A-448222-18 -->
+<div id="yandex_rtb_R-A-448222-18"></div>
+<script>window.yaContextCb.push(()=>{
+		Ya.Context.AdvManager.render({
+		"blockId": "R-A-448222-18",
+		"renderTo": "yandex_rtb_R-A-448222-18"
+		})
+	})
+</script>
--- mainpage.html.php
+++ mainpage.html.php
@@ -603,70 +603,10 @@ include_once __DIR__ . '/admin/adminnews/adminnews.inc.php';
 				</div>	 
 			<?php endforeach; ?>
 		</div>
-		<?php if ($data['mp_difflinks'] == "on") :?>
-			<div class="zen-c-m">
-				<div class = "main-headers">
-					<div class = "main-headers-content">
-						<h2 class="no-link-header">Наш Дзен-канал</h2>
-						<div class = "main-headers-line"></div>				
-					</div>
-				</div>
-				<!-- <div class = "main-headers">
-					<div class = "headers-places"> 
-						<div class = "main-headers-txtplace">Наш Дзен-канал</div>
-					</div>
-					<div class = "main-headers-line"></div>
-				</div> -->
-				<div class="zen-link-m">
-					<a href="https://zen.yandex.ru/imagoz"><img src="./zen-icon.png" alt="Наш Дзен-канал" title="zen.yandex.ru/imagoz"></a>
-				</div>
-			</div>
-			<div class="zen-c-m">
-				<div class = "main-headers">
-					<div class = "main-headers-content">
-						<h2 class="no-link-header">Наш YouTube-канал</h2>
-						<div class = "main-headers-line"></div>				
-					</div>
-				</div>
-				<div class="zen-link-m">
-					<a href="https://www.youtube.com/channel/UCYAlQfGJQC4de8gEmZwF9Yg"><img src="<?php echo '//'.MAIN_URL.'/decoration/youtubem.png';?>" alt="Мы на YouTube!" title="Мы на YouTube!"/></a>
-				</div>
-			</div>
-			<div class="vk-m">
-				<div class = "main-headers">
-					<div class = "main-headers-content">
-						<h2 class="no-link-header">Мы ВКонтакте</h2>
-						<div class = "main-headers-line"></div>				
-					</div>
-				</div>
-				<!-- <div class = "main-headers">
-					<div class = "headers-places"> 
-						<div class = "main-headers-txtplace">Мы ВКонтакте</div>
-					</div>
-					<div class = "main-headers-line"></div>
-				</div> -->
-				<div class="scr-cont">
-				<script type="text/javascript" src="https://vk.com/js/api/openapi.js?169"></script>
-
-					<!-- VK Widget -->
-				<div id="vk_groups"></div>
-				<script type="text/javascript">
-					VK.Widgets.Group("vk_groups", {mode: 3, width: 170, no_cover: 1}, 54027668);
-				</script>
-					
-			</div>
-			<br>
-				<!-- Yandex.RTB R-A-448222-18 -->
-				<div id="yandex_rtb_R-A-448222-18"></div>
-				<script>window.yaContextCb.push(()=>{
-					Ya.Context.AdvManager.render({
-						"blockId": "R-A-448222-18",
-						"renderTo": "yandex_rtb_R-A-448222-18"
-					})
-				})
-				</script>
-		<?php endif; ?>
-		</div>
+		<?php if ($data['mp_difflinks'] == "on") 
+					/*Загрузка ссылок шапки*/
+					include_once MAIN_FILE . '/includes/diffblocks/difflinksmpleft.inc.html.php';
+		?>		
 	</div>
 </div>
 
