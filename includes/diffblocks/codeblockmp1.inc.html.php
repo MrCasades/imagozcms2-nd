3561b1f38acbbdfab10a129c1a49ef6307ad4bfc
MrCasades <casades@list.ru>
2024-07-08 00:09:50 +0300

    new mp code blocks

---
+++ includes/diffblocks/codeblockmp1.inc.html.php
@@ -0,0 +1,18 @@
+<div class = "main-headers">
+	<div class = "main-headers-circle"></div>
+	<div class = "main-headers-content">
+		<h2 class="no-link-header">Новости наших партнёров</h2>
+				<div class = "main-headers-line"></div>				
+	</div>
+</div>
+<div class="gallery-place">
+	<!-- Мир тесен -->
+	<div id="unit_95706"><a href="http://mirtesen.ru/" >Новости МирТесен</a></div>
+	<script type="text/javascript" charset="utf-8">
+		(function() {
+			var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
+			sc.src = '//news.mirtesen.ru/data/js/95706.js'; sc.charset = 'utf-8';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sc, s);
+		}());
+	</script> 
+</div>
--- mainpage.html.php
+++ mainpage.html.php
@@ -50,24 +50,10 @@ include_once __DIR__ . '/admin/adminnews/adminnews.inc.php';
 				Место для рекламы
 		</div> -->
 
-		<div class = "main-headers">
-			<div class = "main-headers-circle"></div>
-			<div class = "main-headers-content">
-				<h2 class="no-link-header">Новости наших партнёров</h2>
-				<div class = "main-headers-line"></div>				
-			</div>
-		</div>
-		<div class="gallery-place">
-			<!-- Мир тесен -->
-			<div id="unit_95706"><a href="http://mirtesen.ru/" >Новости МирТесен</a></div>
-			<script type="text/javascript" charset="utf-8">
-				(function() {
-					var sc = document.createElement('script'); sc.type = 'text/javascript'; sc.async = true;
-					sc.src = '//news.mirtesen.ru/data/js/95706.js'; sc.charset = 'utf-8';
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sc, s);
-				}());
-			</script> 
-		</div>
+		<?php 
+			/*Загрузка блока кода главной страницы 1*/
+			include MAIN_FILE . '/includes/diffblocks/codeblockmp1.inc.html.php';
+		?>
 
 		<?php if ($data['posts'] == "on") :?>
 			<div class = "main-headers">
